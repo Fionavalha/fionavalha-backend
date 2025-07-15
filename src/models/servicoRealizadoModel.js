78da93259c812ea67870b9fcdcf8c8c116b9857d
iranmotaa <iranmota06@gmail.com>
2025-07-15 18:54:04 -0400

    Alteração na rota servicos-realizados

--- src/models/servicoRealizadoModel.js
+++ src/models/servicoRealizadoModel.js
@@ -1,86 +1,158 @@
 import pool from "../config/database.js";
 
 export async function insertServicoRealizado(req) {
-  const sql =
-    "INSERT INTO servicos_realizados(" +
-    "cabelo_id, " +
-    "barba_id, " +
-    "sobrancelha_id, " +
-    "adicional_id, " +
-    "forma_pagamento_id, " +
-    "valor_servico_realizado, " +
-    "data_servico_realizado) " +
-    "VALUES ($1,$2,$3,$4,$5,$6,current_timestamp) " +
-    "RETURNING *";
-  const values = [req.cabelo_id, req.barba_id, req.sobrancelha_id, req.adicional_id, req.forma_pagamento_id, req.valor_servico_realizado];
-  const res = await pool.query(sql, values);
-  return res.rows;
-}
+  const client = await pool.connect();
+  try {
+    await client.query("BEGIN");
 
-export async function selectServicoRealizado(id_servico_realizado) {
-  const sql =
-    "SELECT " +
-    "sr.id_servico_realizado, " +
-    "c.nome_cabelo, " +
-    "b.nome_barba, " +
-    "s.nome_sobrancelha, " +
-    "a.nome_adicional, " +
-    "fp.nome_pagamento, " +
-    "sr.valor_servico_realizado, " +
-    "sr.data_servico_realizado " +
-    "FROM servicos_realizados sr " +
-    "LEFT JOIN cabelos c ON sr.cabelo_id = c.id_cabelo " +
-    "LEFT JOIN barbas b ON sr.barba_id = b.id_barba " +
-    "LEFT JOIN sobrancelhas s ON sr.sobrancelha_id = s.id_sobrancelha " +
-    "LEFT JOIN adicionais a ON sr.adicional_id = a.id_adicional " +
-    "LEFT JOIN formas_pagamento fp ON sr.forma_pagamento_id = fp.id_forma_pagamento " +
-    "WHERE sr.id_servico_realizado = $1";
-  const values = [id_servico_realizado];
-  const res = await pool.query(sql, values);
-  return res.rows;
+    const insertServicoSql = `
+      INSERT INTO servicos_realizados (
+        forma_pagamento_id,
+        valor_total
+      ) VALUES ($1, $2)
+      RETURNING id_servico_realizado
+    `;
+    const valuesServico = [req.forma_pagamento_id, req.valor_total];
+    const result = await client.query(insertServicoSql, valuesServico);
+    const idServico = result.rows[0].id_servico_realizado;
+
+    const insertItemSql = `
+      INSERT INTO itens_servico_realizado (
+        servico_realizado_id,
+        tipo,
+        item_id,
+        valor_item
+      ) VALUES ($1, $2, $3, $4)
+    `;
+
+    for (const item of req.itens) {
+      await client.query(insertItemSql, [idServico, item.tipo, item.item_id, item.valor_item]);
+    }
+
+    await client.query("COMMIT");
+    return { id_servico_realizado: idServico };
+  } catch (error) {
+    await client.query("ROLLBACK");
+    throw error;
+  } finally {
+    client.release();
+  }
 }
 
 export async function selectServicosRealizados() {
-  const sql =
-    "SELECT " +
-    "sr.id_servico_realizado, " +
-    "c.nome_cabelo, " +
-    "b.nome_barba, " +
-    "s.nome_sobrancelha, " +
-    "a.nome_adicional, " +
-    "fp.nome_pagamento, " +
-    "sr.valor_servico_realizado, " +
-    "sr.data_servico_realizado " +
-    "FROM servicos_realizados sr " +
-    "LEFT JOIN cabelos c ON sr.cabelo_id = c.id_cabelo " +
-    "LEFT JOIN barbas b ON sr.barba_id = b.id_barba " +
-    "LEFT JOIN sobrancelhas s ON sr.sobrancelha_id = s.id_sobrancelha " +
-    "LEFT JOIN adicionais a ON sr.adicional_id = a.id_adicional " +
-    "LEFT JOIN formas_pagamento fp ON sr.forma_pagamento_id = fp.id_forma_pagamento " +
-    "ORDER BY sr.data_servico_realizado"
+  const sql = `
+    SELECT
+      sr.id_servico_realizado,
+      STRING_AGG(
+        CASE
+          WHEN i.tipo = 'cabelo' THEN c.nome_cabelo
+          WHEN i.tipo = 'barba' THEN b.nome_barba
+          WHEN i.tipo = 'sobrancelha' THEN s.nome_sobrancelha
+          WHEN i.tipo = 'adicional' THEN a.nome_adicional
+          ELSE 'Outro'
+        END,
+        ' - ' ORDER BY i.tipo DESC
+      ) AS nome_servico,
+      TO_CHAR(sr.data_servico_realizado, 'HH24:MI') AS horario,
+      f.nome_pagamento AS forma_pagamento,
+      sr.valor_total
+    FROM servicos_realizados sr
+    JOIN formas_pagamento f ON f.id_forma_pagamento = sr.forma_pagamento_id
+    JOIN itens_servico_realizado i ON i.servico_realizado_id = sr.id_servico_realizado
+    LEFT JOIN cabelos c ON i.tipo = 'cabelo' AND c.id_cabelo = i.item_id
+    LEFT JOIN barbas b ON i.tipo = 'barba' AND b.id_barba = i.item_id
+    LEFT JOIN sobrancelhas s ON i.tipo = 'sobrancelha' AND s.id_sobrancelha = i.item_id
+    LEFT JOIN adicionais a ON i.tipo = 'adicional' AND a.id_adicional = i.item_id
+    GROUP BY sr.id_servico_realizado, sr.data_servico_realizado, f.nome_pagamento, sr.valor_total
+    ORDER BY sr.data_servico_realizado DESC;
+  `;
+
   const res = await pool.query(sql);
   return res.rows;
 }
 
-export async function updateServicoRealizado(id_servico_realizado, req) {
-  const sql =
-    "UPDATE servicos_realizados SET " +
-    "cabelo_id = $1, " +
-    "barba_id = $2, " +
-    "sobrancelha_id = $3, " +
-    "adicional_id = $4, " +
-    "forma_pagamento_id = $5, " +
-    "valor_servico_realizado = $6 " +
-    "WHERE id_servico_realizado = $7 " +
-    "RETURNING *";
-  const values = [req.cabelo_id, req.barba_id, req.sobrancelha_id, req.adicional_id, req.forma_pagamento_id, req.valor_servico_realizado, id_servico_realizado];
-  const res = await pool.query(sql, values);
+export async function selectServicoRealizado(id) {
+  const sql = `
+    SELECT
+      sr.id_servico_realizado,
+      STRING_AGG(
+        CASE
+          WHEN i.tipo = 'cabelo' THEN c.nome_cabelo
+          WHEN i.tipo = 'barba' THEN b.nome_barba
+          WHEN i.tipo = 'sobrancelha' THEN s.nome_sobrancelha
+          WHEN i.tipo = 'adicional' THEN a.nome_adicional
+          ELSE 'Outro'
+        END,
+        ' - ' ORDER BY i.tipo DESC
+      ) AS nome_servico,
+      TO_CHAR(sr.data_servico_realizado, 'HH24:MI') AS horario,
+      f.nome_pagamento AS forma_pagamento,
+      sr.valor_total
+    FROM servicos_realizados sr
+    JOIN formas_pagamento f ON f.id_forma_pagamento = sr.forma_pagamento_id
+    JOIN itens_servico_realizado i ON i.servico_realizado_id = sr.id_servico_realizado
+    LEFT JOIN cabelos c ON i.tipo = 'cabelo' AND c.id_cabelo = i.item_id
+    LEFT JOIN barbas b ON i.tipo = 'barba' AND b.id_barba = i.item_id
+    LEFT JOIN sobrancelhas s ON i.tipo = 'sobrancelha' AND s.id_sobrancelha = i.item_id
+    LEFT JOIN adicionais a ON i.tipo = 'adicional' AND a.id_adicional = i.item_id
+    WHERE sr.id_servico_realizado = $1
+    GROUP BY sr.id_servico_realizado, sr.data_servico_realizado, f.nome_pagamento, sr.valor_total
+    ORDER BY sr.data_servico_realizado DESC;
+  `;
+  const res = await pool.query(sql, [id]);
   return res.rows;
 }
 
-export async function deleteServicoRealizado(id_servico_realizado) {
-  const sql = "DELETE FROM servicos_realizados WHERE id_servico_realizado = $1";
-  const values = [id_servico_realizado];
-  const res = await pool.query(sql, values);
-  return res.rows;
+export async function updateServicoRealizado(id_servico_realizado, req) {
+  const client = await pool.connect();
+
+  try {
+    await client.query("BEGIN");
+
+    const updateServicoSql = `
+      UPDATE servicos_realizados SET
+        forma_pagamento_id = $1,
+        valor_total = $2
+      WHERE id_servico_realizado = $3
+    `;
+    await client.query(updateServicoSql, [req.forma_pagamento_id, req.valor_total, id_servico_realizado]);
+
+    await client.query("DELETE FROM itens_servico_realizado WHERE servico_realizado_id = $1", [id_servico_realizado]);
+
+    const insertItemSql = `
+      INSERT INTO itens_servico_realizado (
+        servico_realizado_id,
+        tipo,
+        item_id,
+        valor_item
+      ) VALUES ($1, $2, $3, $4)
+    `;
+
+    for (const item of req.itens) {
+      await client.query(insertItemSql, [id_servico_realizado, item.tipo, item.item_id, item.valor_item]);
+    }
+
+    await client.query("COMMIT");
+    return { message: "Serviço atualizado com sucesso" };
+  } catch (error) {
+    await client.query("ROLLBACK");
+    throw error;
+  } finally {
+    client.release();
+  }
+}
+
+export async function deleteServicoRealizado(id) {
+  const client = await pool.connect();
+  try {
+    await client.query("BEGIN");
+    await client.query("DELETE FROM itens_servico_realizado WHERE servico_realizado_id = $1", [id]);
+    await client.query("DELETE FROM servicos_realizados WHERE id_servico_realizado = $1", [id]);
+    await client.query("COMMIT");
+  } catch (error) {
+    await client.query("ROLLBACK");
+    throw error;
+  } finally {
+    client.release();
+  }
 }
