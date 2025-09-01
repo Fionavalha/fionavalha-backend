aab73c62cb8770bc301a00fc72d5a2d8b929b447
iranmotaa <iranmota06@gmail.com>
2025-09-01 19:38:11 -0400

    Criação de despesas e receitas

--- src/models/despesaModel.js
+++ src/models/despesaModel.js
@@ -27,27 +27,49 @@ export async function selectDespesa(id_despesa) {
 }
 
 export async function selectDespesas(req) {
-  const sql = ` 
-    SELECT id_despesa, nome_despesa, valor_despesa, 
-      TO_CHAR(data_despesa, 'YYYY-MM-DD') AS data_despesa,
-      CASE 
-        WHEN fixa = false THEN 'N'
-        ELSE 'S'
-      END AS fixa
-    FROM despesas 
-    WHERE data_despesa BETWEEN $1 AND $2`;
+  let resDespesas, resTotal;
 
-  const sqlTotalGeral = `
-    SELECT
-      COUNT(*)::INTEGER AS qtd,
-      SUM(valor_despesa)::NUMERIC(10,2) AS valor_total
-    FROM despesas
-    WHERE data_despesa BETWEEN $1 AND $2`;
+  if (req.data_inicial && req.data_final !== "") {
+    const sql = ` 
+        SELECT id_despesa, nome_despesa, valor_despesa, 
+          TO_CHAR(data_despesa, 'YYYY-MM-DD') AS data_despesa,
+          CASE 
+            WHEN fixa = false THEN 'N'
+            ELSE 'S'
+          END AS fixa
+        FROM despesas 
+        WHERE data_despesa BETWEEN $1 AND $2`;
 
-  const values = [req.data_inicial, req.data_final];
+    const sqlTotalGeral = `
+        SELECT
+          COUNT(*)::INTEGER AS qtd,
+          SUM(valor_despesa)::NUMERIC(10,2) AS valor_total
+        FROM despesas
+        WHERE data_despesa BETWEEN $1 AND $2`;
 
-  const resDespesas = await pool.query(sql, values);
-  const resTotal = await pool.query(sqlTotalGeral, values);
+    const values = [req.data_inicial, req.data_final];
+
+    resDespesas = await pool.query(sql, values);
+    resTotal = await pool.query(sqlTotalGeral, values);
+  } else {
+    const sql = ` 
+      SELECT id_despesa, nome_despesa, valor_despesa, 
+        TO_CHAR(data_despesa, 'YYYY-MM-DD') AS data_despesa,
+        CASE 
+          WHEN fixa = false THEN 'N'
+          ELSE 'S'
+        END AS fixa
+      FROM despesas`;
+
+    const sqlTotalGeral = `
+      SELECT
+        COUNT(*)::INTEGER AS qtd,
+        SUM(valor_despesa)::NUMERIC(10,2) AS valor_total
+      FROM despesas`;
+
+    resDespesas = await pool.query(sql);
+    resTotal = await pool.query(sqlTotalGeral);
+  }
   return {
     despesas: resDespesas.rows,
     total_geral: resTotal.rows[0],
--- src/models/receitasModel.js
+++ src/models/receitasModel.js
@@ -5,71 +5,70 @@ export async function selectReceitas(req) {
     SELECT
       c.nome_cabelo AS nome,
       COUNT(*)::INTEGER AS qtd,
-      SUM(i.valor_item)::NUMERIC(10,2) AS valor_total,
-      sr.data_servico_realizado::date AS data_servico_realizado
+      SUM(i.valor_item)::NUMERIC(10,2) AS valor_total
     FROM itens_servico_realizado i
     JOIN cabelos c ON c.id_cabelo = i.item_id
-    JOIN servicos_realizados sr ON sr.id_servico_realizado = i.item_id
+    JOIN servicos_realizados sr ON sr.id_servico_realizado = i.servico_realizado_id
     WHERE i.tipo = 'cabelo'
-    AND sr.data_servico_realizado::date BETWEEN $1 AND $2
-    GROUP BY c.nome_cabelo, sr.data_servico_realizado::date
+      AND sr.data_servico_realizado::date BETWEEN $1 AND $2
+    GROUP BY c.nome_cabelo
 
   UNION ALL
 
-  SELECT
-    b.nome_barba AS nome,
-    COUNT(*)::INTEGER AS qtd,
-    SUM(i.valor_item)::NUMERIC(10,2) AS valor_total,
-    sr.data_servico_realizado::date AS data_servico_realizado
-  FROM itens_servico_realizado i
-  JOIN barbas b ON b.id_barba = i.item_id
-  JOIN servicos_realizados sr ON sr.id_servico_realizado = i.item_id
-  WHERE i.tipo = 'barba'
-  AND sr.data_servico_realizado::date BETWEEN $1 AND $2
-  GROUP BY b.nome_barba, sr.data_servico_realizado::date
+    SELECT
+      b.nome_barba AS nome,
+      COUNT(*)::INTEGER AS qtd,
+      SUM(i.valor_item)::NUMERIC(10,2) AS valor_total
+    FROM itens_servico_realizado i
+    JOIN barbas b ON b.id_barba = i.item_id
+    JOIN servicos_realizados sr ON sr.id_servico_realizado = i.servico_realizado_id
+    WHERE i.tipo = 'barba'
+      AND sr.data_servico_realizado::date BETWEEN $1 AND $2
+    GROUP BY b.nome_barba
 
   UNION ALL
 
-  SELECT
-    s.nome_sobrancelha AS nome,
-    COUNT(*)::INTEGER AS qtd,
-    SUM(i.valor_item)::NUMERIC(10,2) AS valor_total,
-    sr.data_servico_realizado::date AS data_servico_realizado
-  FROM itens_servico_realizado i
-  JOIN sobrancelhas s ON s.id_sobrancelha = i.item_id
-  JOIN servicos_realizados sr ON sr.id_servico_realizado = i.item_id
-  WHERE i.tipo = 'sobrancelha'
-  AND sr.data_servico_realizado::date BETWEEN $1 AND $2
-  GROUP BY s.nome_sobrancelha, sr.data_servico_realizado::date
+    SELECT
+      s.nome_sobrancelha AS nome,
+      COUNT(*)::INTEGER AS qtd,
+      SUM(i.valor_item)::NUMERIC(10,2) AS valor_total
+    FROM itens_servico_realizado i
+    JOIN sobrancelhas s ON s.id_sobrancelha = i.item_id
+    JOIN servicos_realizados sr ON sr.id_servico_realizado = i.servico_realizado_id
+    WHERE i.tipo = 'sobrancelha'
+      AND sr.data_servico_realizado::date BETWEEN $1 AND $2
+    GROUP BY s.nome_sobrancelha
 
   UNION ALL
 
-  SELECT
-    a.nome_adicional AS nome,
-    COUNT(*)::INTEGER AS qtd,
-    SUM(i.valor_item)::NUMERIC(10,2) AS valor_total,
-    sr.data_servico_realizado::date AS data_servico_realizado
-  FROM itens_servico_realizado i
-  JOIN adicionais a ON a.id_adicional = i.item_id
-  JOIN servicos_realizados sr ON sr.id_servico_realizado = i.item_id
-  WHERE i.tipo = 'adicional'
-  AND sr.data_servico_realizado::date BETWEEN $1 AND $2
-  GROUP BY a.nome_adicional, sr.data_servico_realizado::date
+    SELECT
+      a.nome_adicional AS nome,
+      COUNT(*)::INTEGER AS qtd,
+      SUM(i.valor_item)::NUMERIC(10,2) AS valor_total
+    FROM itens_servico_realizado i
+    JOIN adicionais a ON a.id_adicional = i.item_id
+    JOIN servicos_realizados sr ON sr.id_servico_realizado = i.servico_realizado_id
+    WHERE i.tipo = 'adicional'
+      AND sr.data_servico_realizado::date BETWEEN $1 AND $2
+    GROUP BY a.nome_adicional
 
   ORDER BY nome
   `;
 
   const sqlTotalGeral = `
-  SELECT
-    COUNT(*)::INTEGER AS qtd,
-    SUM(valor_item)::NUMERIC(10,2) AS valor_total
-  FROM itens_servico_realizado;
-`;
+    SELECT
+      COUNT(*)::INTEGER AS qtd,
+      SUM(valor_item)::NUMERIC(10,2) AS valor_total
+    FROM itens_servico_realizado i
+    JOIN servicos_realizados s 
+      ON s.id_servico_realizado = i.servico_realizado_id
+    WHERE s.data_servico_realizado::date BETWEEN $1 AND $2;
+  `;
 
   const values = [req.data_inicial, req.data_final];
 
   const resCortes = await pool.query(sql, values);
-  const resTotal = await pool.query(sqlTotalGeral);
+  const resTotal = await pool.query(sqlTotalGeral, values);
 
   return {
     cortes: resCortes.rows,
