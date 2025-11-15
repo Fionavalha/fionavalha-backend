24fd000c14ee8322ed878d782649f4e04463d345
iranmotaa <iranmota06@gmail.com>
2025-11-15 19:45:10 -0400

    :sparkles: feat: Cors configurado

--- src/app.js
+++ src/app.js
@@ -1,10 +1,14 @@
 import express from "express";
-import cors from "cors"
+import cors from "cors";
 import routes from "./routes/index.js";
 
 const app = express();
 
-app.use(cors())
+app.use(
+  cors({
+    origin: "https://www.fionavalha.com.br",
+  })
+);
 
 app.use(express.json());
 
